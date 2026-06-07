4cb7294ada99a80aa7d02d7a000f0e43b470882e
Shreyas <shreyasofficial2904@gmail.com>
2026-06-07 19:18:58 +0530

    Sample camera test script - opencv2

---
+++ demo_runs/camera_test.py
@@ -0,0 +1,21 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Camera not working")
+        break
+
+    cv2.imshow("UniGuard Camera Feed", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
